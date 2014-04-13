dc4c23eff8b1de991d05e8f9a7b629d848c13205
amednin <amed.nin@gmail.com>
2014-04-12 21:51:48 -0400

    Maps abstracting

--- baseproject/src/Air/WebAppBundle/Resources/public/js/application/main-app.js
+++
@@ -1,3 +0,0 @@
-$('document').on('ready', function (){
-    window.wayranita = {};    
-});
--- baseproject/src/Air/WebAppBundle/Resources/public/js/application/views/MapView.js
+++ baseproject/src/Air/WebAppBundle/Resources/public/js/application/views/MapView.js
@@ -1,8 +1,35 @@
-window.wayranita.views.mapView = Backbone.extend.View({
+var MapView = Backbone.View.extend({
     el: $('body'),
     
+    mapModel: null,
+    
     initialize: function (){
-        console.log('init');
+        this.mapModel = new MapModel();
+        
+    },
+    
+    renderMap: function (){
+        var self = this;
+        function initializeMap(){
+            var mapOptions = {
+                center: self.mapModel.createLatLngObj(),
+                zoom: self.mapModel.get('zoom'),
+                mapTypeId: self.mapModel.get('mapTypeId')
+            };
+            var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+            
+            var imageBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(40.712216,-74.22655),
+      new google.maps.LatLng(40.773941,-74.12544));
+      
+      var oldmap = new google.maps.GroundOverlay(
+        "http://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg",
+        imageBounds);
+    oldmap.setMap(map);
+        }
+      google.maps.event.addDomListener(window, 'load', initializeMap);
+      
+      
     }
 });
 
